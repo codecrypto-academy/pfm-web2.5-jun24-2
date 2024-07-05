14417c57b7e6d8c5307f1abfcd604a625451d153
Javier Lansky <jmlansky@hotmail.com>
2024-07-05 18:25:50 +0200

    update back changes

--- back/src/app.ts
+++ back/src/app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import routes from './routes';
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use('/api', routes);
@@ -11,3 +13,4 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
--- back/src/routes.ts
+++ back/src/routes.ts
@@ -4,11 +4,23 @@ import { containersInfo } from './dockerService';
 
 const router = express.Router();
 
+//Add networks or nodes
 router.post('/network', (req: Request, res: Response) => createNetwork(req, res));
+//router.post('/network/:id/node', (req: Request, res: Response) => createNode(req, res));
+
+// Network endpoints
 router.post('/network/:id/stop', (req: Request, res: Response) => stopNetwork(req, res));
-// router.post('/network/:id/reset', (req: Request, res: Response) => resetNetwork(req, res));
 router.post('/network/:id/start', (req: Request, res: Response) => startNetwork(req, res));
+//router.post('/network/:id/restart', (req: Request, res: Response) => restartNetwork(req, res));
 router.get('/networks', (req: Request, res: Response) => listNetworks(req, res));
+
+// nodes endpoints
+// router.post('/node/:id/stop', (req: Request, res: Response) => stopNode(req, res));
+// router.post('/node/:id/start', (req: Request, res: Response) => startNetwork(req, res));
+// router.post('/node/:id/restart', (req: Request, res: Response) => restartNetwork(req, res));
+// router.get('/network/:id/nodes', (req: Request, res: Response) => listNetworks(req, res));
+
+
 router.get('/containers', (req: Request, res: Response) => containersInfo(req, res));
 
 export default router;
