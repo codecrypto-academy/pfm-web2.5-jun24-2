757327bce01eb13c389aa669c06d83be1b12be98
Javier Lansky <jmlansky@hotmail.com>
2024-07-08 21:32:45 +0200

    crear network

--- back/src/routes.ts
+++ back/src/routes.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from 'express';
-import { createNetwork, stopNetwork, startNetwork, listNetworks } from './services/networkService';
+import { createNetwork, stopNetwork, startNetwork, listNetworks, restartNetwork, listNodes } from './services/networkService';
 import { containersInfo } from './services/dockerService';
 
 const router = express.Router();
@@ -11,14 +11,14 @@ router.post('/network', (req: Request, res: Response) => createNetwork(req, res)
 // Network endpoints
 router.post('/network/:id/stop', (req: Request, res: Response) => stopNetwork(req, res));
 router.post('/network/:id/start', (req: Request, res: Response) => startNetwork(req, res));
-//router.post('/network/:id/restart', (req: Request, res: Response) => restartNetwork(req, res));
+router.post('/network/:id/restart', (req: Request, res: Response) => restartNetwork(req, res));
 router.get('/networks', (req: Request, res: Response) => listNetworks(req, res));
 
 // nodes endpoints
 // router.post('/node/:id/stop', (req: Request, res: Response) => stopNode(req, res));
 // router.post('/node/:id/start', (req: Request, res: Response) => startNetwork(req, res));
 // router.post('/node/:id/restart', (req: Request, res: Response) => restartNetwork(req, res));
-// router.get('/network/:id/nodes', (req: Request, res: Response) => listNetworks(req, res));
+router.get('/network/:id/nodes', (req: Request, res: Response) => listNodes(req, res));
 
 
 router.get('/containers', (req: Request, res: Response) => containersInfo(req, res));
