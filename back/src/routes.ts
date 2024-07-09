c4be0dc01af156ca34db55a3eb1c0e287bf378b3
Javier Lansky <jmlansky@hotmail.com>
2024-07-09 17:47:33 +0200

    refactor + nodeService + add new node + remove node

--- back/src/routes.ts
+++ back/src/routes.ts
@@ -1,13 +1,13 @@
 import express, { Request, Response } from 'express';
-import { createNetwork, stopNetwork, startNetwork, listNetworks, restartNetwork, listNodes } from './services/networkService';
+import { createNetwork, stopNetwork, startNetwork, listNetworks, restartNetwork, listNodes, addNewNodes } from './services/networkService';
 import { containersInfo } from './services/dockerService';
-import { restartNode, startNode, stopNode } from './services/nodeService';
+import { deleteNode, restartNode, startNode, stopNode } from './services/nodeService';
 
 const router = express.Router();
 
 //Add networks or nodes
 router.post('/network', (req: Request, res: Response) => createNetwork(req, res));
-//router.post('/network/:id/node', (req: Request, res: Response) => createNode(req, res));
+router.post('/network/:id/nodes', (req: Request, res: Response) => addNewNodes(req, res));
 
 // Network endpoints
 router.post('/network/:id/stop', (req: Request, res: Response) => stopNetwork(req, res));
@@ -20,7 +20,7 @@ router.post('/node/:id/stop', (req: Request, res: Response) => stopNode(req, res
 router.post('/node/:id/start', (req: Request, res: Response) => startNode(req, res));
 router.post('/node/:id/restart', (req: Request, res: Response) => restartNode(req, res));
 router.get('/network/:id/nodes', (req: Request, res: Response) => listNodes(req, res));
-
+router.delete('/node/:id/', (req: Request, res: Response) => deleteNode(req, res));
 
 router.get('/containers', (req: Request, res: Response) => containersInfo(req, res));
 
